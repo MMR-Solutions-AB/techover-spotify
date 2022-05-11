dab662115a3167bc8885af2f01bb25e3a001e6f5
Nabil <hayeknabil214@gmail.com>
2022-05-11 16:13:48 +0200

    V1 - Tuesday

---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Box, Divider } from '@mui/material';
+import SideNavOption from '../SideNavOption/SideNavOption';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+export default function SideNav({ playlists }) {
+  const renderPlaylists = () => {
+    return playlists.map((playlist) => {
+      return <SideNavOption key={playlist.playlistId} name={playlist.name} />;
+    });
+  };
+
+  return (
+    <Box
+      sx={{
+        bgcolor: 'black',
+        width: 275,
+        height: '100vh',
+        display: { xs: 'none', md: 'block' },
+      }}
+    >
+      <img
+        style={{ width: 130, padding: '24px 0px 24px 24px' }}
+        src='/Spotify_Logo.png'
+        alt='logo'
+      />
+      <SideNavOption name='Home' Icon={HomeOutlinedIcon} />
+      <SideNavOption name='Search' Icon={SearchOutlinedIcon} />
+      <Divider sx={{ backgroundColor: '#ffffff30', margin: '12px 24px' }} />
+      {renderPlaylists()}
+    </Box>
+  );
+}
